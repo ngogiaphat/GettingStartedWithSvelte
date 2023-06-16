f13c4f05b361d8e4183986a153aa90b387ce1ec4
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-17 01:15:40 +0700

    started create

---
+++ src/routes/posts/create/+page.svelte
@@ -0,0 +1,6 @@
+<h1>
+	Create new post
+</h1>
+<p>
+	Create a new post here.
+</p>
---
+++ src/routes/posts/create/+page.ts
@@ -0,0 +1,6 @@
+import {error} from "@sveltejs/kit"; 
+import type {PageLoad} from "./$types";
+export const load: PageLoad = async() => {
+	// throw new Error("Yikes !!!");
+	throw error(404, {message: "Yikes !!!"});
+};
