318b8a0a4dcd393f2628fa22ac83f8b50c2cb352
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-16 22:04:36 +0700

    change the docs

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -127,7 +127,6 @@ declare module '$env/static/private' {
 declare module '$env/static/public' {
 	
 }
-
 /**
  * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/master/packages/adapter-node) (or running [`vite preview`](https://kit.svelte.dev/docs/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env).
  * 
