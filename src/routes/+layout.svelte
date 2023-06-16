d890e4a4f8b4b2a8f18add097c67b48b5c089db7
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-17 01:16:55 +0700

    create routes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang = "ts">
+	import '../app.css';
+</script>
+<nav>
+	<ul>
+		<li>
+			<a href = "/">Home</a>
+		</li>
+		<li>
+			<a href = "/posts">Posts</a>
+		</li>
+	</ul>
+</nav>
+<slot/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang = "ts">
+	console.log('Hello');
+</script>
+<nav>
+	<a href = "/">Home</a>
+	<a href = "/posts">Posts</a>
+</nav>
+<h1>
+	Home Page
+</h1>
