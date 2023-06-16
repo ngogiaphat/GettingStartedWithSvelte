5408f6352f71a52af47b8a2e924818fcec085063
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-17 01:19:24 +0700

    change the package

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import {sveltekit} from '@sveltejs/kit/vite';
 /**@type {import('vite').UserConfig}*/
 const config = {
 	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test, spec}.{js, ts}'],
+	},
 };
 export default config;
