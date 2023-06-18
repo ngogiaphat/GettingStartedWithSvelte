7dfc7e323429d61a726e5070e4e2cba79075a7aa
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-18 17:08:51 +0700

    defineConfig

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import {sveltekit} from '@sveltejs/kit/vite';
-/**@type {import('vite').UserConfig}*/
+/** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test, spec}.{js, ts}'],
-	},
+  plugins: [sveltekit()],
 };
 export default config;
+// import {defineConfig} from 'vitest/config';
+// import {sveltekit} from '@sveltejs/kit/vite';
+// export default defineConfig({
+// 	plugins: [sveltekit()],
+// 	test: {
+// 		include: ['src/**/*.{test, spec}.{js, ts}']
+// 	},
+// });
