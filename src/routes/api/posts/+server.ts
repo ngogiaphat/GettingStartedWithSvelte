ad01b1537c064f80efb93149d7f0177567bbdeee
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-18 17:12:21 +0700

    $types and $lib is not working and them is damned !!!!! fucking shit

---
+++ src/routes/api/posts/+server.ts
@@ -0,0 +1,7 @@
+import db from './$lib';
+import {json} from "@sveltejs/kit";
+import type {RequestHandler} from './$types';
+export const GET: RequestHandler = async() => {
+	const posts = await db.post.findMany();
+	return json(posts);
+};
