6066efc11f10fc5682273374e383f59d3b02d379
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-19 17:47:58 +0700

    server is error !!!

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,7 +1,14 @@
-import db from './$lib';
+import db from './$lib/database';
 import {json} from "@sveltejs/kit";
 import type {RequestHandler} from './$types';
-export const GET: RequestHandler = async() => {
-	const posts = await db.post.findMany();
+export const GET: RequestHandler = async({url, route, params}) => {
+	const limit = Number(url.searchParams.get('limit') ?? 30);
+	const order = url.searchParams.get('order') ?? 'asc';
+	const posts = await db.post.findMany({
+		orderBy: {
+			id: order,
+		},
+		take: limit,
+	});
 	return json(posts);
 };
