13599202d625975d7dde5ee2390655c27ea6fb22
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-21 19:22:16 +0700

    error /$lib/database

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,14 +1,15 @@
 import db from './$lib/database';
 import {json} from "@sveltejs/kit";
 import type {RequestHandler} from './$types';
-export const GET: RequestHandler = async({url, route, params}) => {
+export const GET: RequestHandler = async({url}) => {
+	console.log(url);
 	const limit = Number(url.searchParams.get('limit') ?? 30);
 	const order = url.searchParams.get('order') ?? 'asc';
 	const posts = await db.post.findMany({
-		orderBy: {
+		orderBy : {
 			id: order,
+			take: limit, 
 		},
-		take: limit,
 	});
 	return json(posts);
 };
