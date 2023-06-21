99f2994ff865b1c0d28b267d6337adf35cdfb78a
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-21 19:24:33 +0700

    ./$types

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,28 @@
-<script lang = "ts">
+<script lang="ts">
 	import type {PageData} from './$types';
+  import {invalidate, invalidateAll} from '$app/navigation';
 	export let data: PageData;
 	$: ({posts} = data);
+	function rerunLoadFunction(){
+		invalidate('posts');
+		invalidate('api/posts');
+		invalidate(url => url.href.includes('posts'));
+		invalidateAll();
+	}
 </script>
 <h1>Posts</h1>
-<p>Showing {posts.length} posts</p>
+<button on:click = {rerunLoadFunction}>
+	Rerun
+</button>
+<p>Showing 
+	{posts.length} posts
+</p>
 {#each posts as {slug, title}}
 	<ul>
 		<li>
-			<a href = "/posts/{slug}">{title}</a>
+			<a href = "/posts/{slug}">
+				{title}
+			</a>
 		</li>
 	</ul>
 {/each}
