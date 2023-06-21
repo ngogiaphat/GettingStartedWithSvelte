e74f57b60440f4b6a114b76716d3bc3826a08ae6
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-21 19:24:05 +0700

    error ./$types

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,9 @@
 import type {PageLoad} from "./$types";
 import type {Post} from "@prisma/client";
-export const load: PageLoad = async({fetch}) => {
-	const response = await fetch('/api/posts');
+export const load: PageLoad = async({fetch, depends}) => {
+	const random = Math.round(Math.random() *30)
+	const response = await fetch(`/api/posts?limit = ${random}`);
 	const posts : Post[] = await response.json();
+	depends('posts');
 	return {posts};
 };
