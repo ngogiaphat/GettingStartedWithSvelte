349e326fea02ab4e5ca95e58dd9c0dfdf2dd301b
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-21 19:26:23 +0700

    process

---
+++ src/routes/+form.svelte
@@ -0,0 +1,10 @@
+<h1>
+	Forms
+</h1>
+<form method = "GET" action = "/login">
+	<input type = "text" name = "user"/>
+	<input type = "password" name = "password"/>
+	<button type = "submit">
+		Login
+	</button>
+</form>
